0ef315d6d66f2e306eb7e96a48b09957df304178
naoto67 <uver.com67@gmail.com>
2020-02-17 01:46:10 +0900

    fix UserRepo to userRepo in user.go under infra

--- src/infrastructure/user.go
+++ src/infrastructure/user.go
@@ -6,22 +6,22 @@ import (
 	"github.com/naoto67/clean-architecture/src/infrastructure/memory"
 )
 
-type UserRepository struct {
+type userRepository struct {
 	Memory *memory.Memory
 }
 
 func NewUserRepository(m *memory.Memory) repository.UserRepository {
-	return UserRepository{Memory: m}
+	return userRepository{Memory: m}
 }
 
-func (repository UserRepository) FindByName(name string) (*model.User, error) {
+func (repository userRepository) FindByName(name string) (*model.User, error) {
 	return repository.Memory.FindByName(name)
 }
 
-func (repository UserRepository) Save(user model.User) error {
+func (repository userRepository) Save(user model.User) error {
 	return repository.Memory.Save(user)
 }
 
-func (repository UserRepository) FindAll() ([]model.User, error) {
+func (repository userRepository) FindAll() ([]model.User, error) {
 	return repository.Memory.FindAll()
 }
